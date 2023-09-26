bdeec9071918b26a20aefe197ef23cbc86ea7a5d
Gianluca Recchia <g.recchia@desotech.it>
2023-09-26 18:13:57 +0200

    Restore javascript file

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,94 +1,63 @@
 function removeAllChildren(element) {
 	let first = element.firstElementChild;
 	while (first) {
-	  first.remove();
-	  first = element.firstElementChild;
+		first.remove();
+		first = element.firstElementChild;
 	}
-  }
-  
-  function refreshHeader(table) {
-	const existingHeader = table.querySelector('.table-header');
-	if (existingHeader) {
-	  existingHeader.remove();
-	}
-  
-	const headerRow = document.createElement('li');
-	headerRow.classList.add('table-header');
-	const rankHeader = document.createElement('div');
-	rankHeader.classList.add('col', 'col-1');
-	rankHeader.setAttribute('data-label', 'Rank');
-	rankHeader.textContent = 'Rank';
-	const nameHeader = document.createElement('div');
-	nameHeader.classList.add('col', 'col-2');
-	nameHeader.setAttribute('data-label', 'Player');
-	nameHeader.textContent = 'Player';
-	const scoreHeader = document.createElement('div');
-	scoreHeader.classList.add('col', 'col-3');
-	scoreHeader.setAttribute('data-label', 'Score');
-	scoreHeader.textContent = 'Score';
-	const dateHeader = document.createElement('div');
-	dateHeader.classList.add('col', 'col-4');
-	dateHeader.setAttribute('data-label', 'Date');
-	dateHeader.textContent = 'Date';
-	headerRow.appendChild(rankHeader);
-	headerRow.appendChild(nameHeader);
-	headerRow.appendChild(scoreHeader);
-	headerRow.appendChild(dateHeader);
-	table.appendChild(headerRow);
-  }
-  
-  function refreshLeaderboardRows(table, data) {
+}
+
+function refreshHeader(table) {
+	const row = document.createElement('tr');
+	row.classList.add('highscore-table-header');
+	const rankHeader = document.createElement('th');
+	const nameHeader = document.createElement('th');
+	const scoreHeader = document.createElement('th');
+	rankHeader.appendChild(document.createTextNode('Rank'));
+	nameHeader.appendChild(document.createTextNode('Name'));
+	scoreHeader.appendChild(document.createTextNode('Score'));
+	row.appendChild(rankHeader);
+	row.appendChild(nameHeader);
+	row.appendChild(scoreHeader);
+	table.appendChild(row);
+}
+
+function refreshLeaderboardRows(table, data) {
 	refreshHeader(table);
 	data.forEach((item, index) => {
-	  const rank = index + 1;
-	  const name = item.name;
-	  const date = item.date;
-	  const score = item.score;
-  
-	  const row = document.createElement('li');
-	  row.classList.add('table-row');
-	  const rankCell = document.createElement('div');
-	  rankCell.classList.add('col', 'col-1');
-	  rankCell.setAttribute('data-label', 'Rank');
-	  rankCell.textContent = rank;
-	  const nameCell = document.createElement('div');
-	  nameCell.classList.add('col', 'col-2');
-	  nameCell.setAttribute('data-label', 'Player');
-	  nameCell.textContent = name;
-	  const scoreCell = document.createElement('div');
-	  scoreCell.classList.add('col', 'col-3');
-	  scoreCell.setAttribute('data-label', 'Score');
-	  scoreCell.textContent = score;
-	  const dateCell = document.createElement('div');
-	  dateCell.classList.add('col', 'col-4');
-	  dateCell.setAttribute('data-label', 'Date');
-	  dateCell.textContent = date;
-	  row.appendChild(rankCell);
-	  row.appendChild(nameCell);
-	  row.appendChild(scoreCell);
-	  row.appendChild(dateCell);
-	  table.appendChild(row);
+		const rank = index + 1;
+		const name = item.name;
+		const score = item.score;
+
+		const row = document.createElement('tr');
+		row.classList.add('highscore-table-row');
+		const rankCell = document.createElement('td');
+		const nameCell = document.createElement('td');
+		const scoreCell = document.createElement('td');
+		rankCell.appendChild(document.createTextNode(rank));
+		nameCell.appendChild(document.createTextNode(name));
+		scoreCell.appendChild(document.createTextNode(score));
+		row.appendChild(rankCell);
+		row.appendChild(nameCell);
+		row.appendChild(scoreCell);
+		table.appendChild(row);
 	});
-  }
-  
-  async function refreshLeaderboard(table) {
-	try {
-	  const response = await fetch('/api/highscore');
-	  if (!response.ok) {
-		throw new Error('Network response was not ok');
-	  }
-	  const data = await response.json();
-	  removeAllChildren(table);
-	  refreshLeaderboardRows(table, data);
-	} catch (error) {
-	  console.error(`Download error: ${error.message}`);
-	}
-  }
-  
-  addEventListener('load', () => {
+}
+
+async function refreshLeaderboard(table) {
+	fetch('/api/highscore')
+		.then(response => response.json())
+		.then(data => {
+			removeAllChildren(table);
+			refreshLeaderboardRows(table, data);
+		})
+		.catch(error => alert(`Download error: ${error.message}`));
+}
+
+
+addEventListener('load', () => {
 	const root = document.getElementById('root');
-	const leaderboardTable = document.createElement('ul');
-	leaderboardTable.classList.add('responsive-table');
+	const leaderboardTable = document.createElement('table');
+	leaderboardTable.setAttribute('id', 'highscore-table');
 	root.appendChild(leaderboardTable);
-	refreshLeaderboard(leaderboardTable);
-  });
+	setTimeout(() => { refreshLeaderboard(leaderboardTable); }, 500);
+});
